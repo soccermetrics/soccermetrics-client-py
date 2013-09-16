e1ee2e8ee308c7e55885078def8f4b367a047774
Howard Hamilton <hhamilton@soccermetrics.net>
2013-09-16 01:02:53 -0400

    Edit example source code to reflect new API functionality

--- examples/example_conditions.py
+++ examples/example_conditions.py
@@ -6,30 +6,47 @@
 
 from soccermetrics.rest import SoccermetricsRestClient
 
-# Create a SoccermetricsRestClient object.  This call assumes that
-# SOCCERMETRICS_APP_ID and SOCCERMETRICS_APP_KEY are in your environment
-# variables, which we recommend.
-client = SoccermetricsRestClient()
+# A closure that we've written to page through the resource
+# representation.  We'll add the functionality in a future
+# release of the client so that you won't have to write this,
+# but we want to make sure you can run this example now.
+def iter(resp):
+    while True:
+        yield (resp.data)
+        if not resp.meta.next:
+            raise StopIteration
+        else:
+            resp = client.link.get(resp.meta.next)
 
-# Get match info data from all matches.  We do this by first querying
-# for all of Everton's home matches, then by querying their away matches.
-#
-# The result is a list, so we can join them together by concatenation.
-# (Order doesn't matter, we're going to sort the result in a sec.)
-matches = client.match.information.get(home_team_name="Everton") + \
-          client.match.information.get(away_team_name="Everton")
+if __name__ == "__main__":
 
-# Results from the API are unsorted, so sort the results by match date.
-# You can also sort by matchday but not all matches are played in order.
-sorted_matches = sorted(matches, key=lambda k: k.match_date)
+    # Create a SoccermetricsRestClient object.  This call assumes that
+    # SOCCERMETRICS_APP_ID and SOCCERMETRICS_APP_KEY are in your environment
+    # variables, which we recommend.
+    client = SoccermetricsRestClient()
 
-# Now we can iterate over the sorted match list (and make sure you are
-# sorting over the sorted match list!)  For every match record, retrieve
-# its match condition record, which contains weather conditions.
-#
-# Match date and kickoff time are in the match information record, but
-# temperatures are in the match condition record.
-print "Match Date,Kickoff Time,Kickoff Temp"
-for match in sorted_matches:
-    condition = client.link.get(match.link.conditions)
-    print "%s,%s,%2.1f" % (match.match_date, match.kickoff_time, condition.kickoff_temp)
+    # Get match info data from all matches.  We do this by first querying
+    # for all of Everton's home matches, then by querying their away matches.
+    #
+    # The result is a list, so we can join them together by concatenation.
+    # (Order doesn't matter, we're going to sort the result in a sec.)
+    matches = []
+    for key in ['home_team_name','away_team_name']:
+        param = {key: 'Everton'}
+        for page in iter(client.match.information.get(**param)):
+            matches.extend(page)
+
+    # Results from the API are unsorted, so sort the results by match date.
+    # You can also sort by matchday but not all matches are played in order.
+    sorted_matches = sorted(matches, key=lambda k: k.match_date)
+
+    # Now we can iterate over the sorted match list (and make sure you are
+    # sorting over the sorted match list!)  For every match record, retrieve
+    # its match condition record, which contains weather conditions.
+    #
+    # Match date and kickoff time are in the match information record, but
+    # temperatures are in the match condition record.
+    print "Match Date,Kickoff Time,Kickoff Temp"
+    for match in sorted_matches:
+        condition = client.link.get(match.link.conditions).data
+        print "%s,%s,%2.1f" % (match.match_date, match.kickoff_time, condition.kickoff_temp)
--- examples/example_rotations.py
+++ examples/example_rotations.py
@@ -6,47 +6,66 @@
 
 from soccermetrics.rest import SoccermetricsRestClient
 
-# Create a SoccermetricsRestClient object.  This call assumes that
-# SOCCERMETRICS_APP_ID and SOCCERMETRICS_APP_KEY are in your environment
-# variables, which we recommend.
-client = SoccermetricsRestClient()
+# A closure that we've written to page through the resource
+# representation.  We'll add the functionality in a future
+# release of the client so that you won't have to write this,
+# but we want to make sure you can run this example now.
+def iter(resp):
+    while True:
+        yield (resp.data)
+        if not resp.meta.next:
+            raise StopIteration
+        else:
+            resp = client.link.get(resp.meta.next)
 
-# Create STARTING_XI constant and set it to 11.
-# It's not necessary, but we don't like to hard-code numbers.
-STARTING_XI = 11
+if __name__ == "__main__":
 
-# Get unique IDs of all of Liverpool's matches.  We want the matches in order,
-# so we do a few things:
-#   (1) We query all of Liverpool's home matches, then all of their away matches
-#       and join the results together.
-#   (2) We sort the results by match date.
-matches = client.match.information.get(home_team_name="Liverpool") + \
-          client.match.information.get(away_team_name="Liverpool")
-sorted_matches = sorted(matches, key=lambda k: k.match_date)
+    # Create a SoccermetricsRestClient object.  This call assumes that
+    # SOCCERMETRICS_APP_ID and SOCCERMETRICS_APP_KEY are in your environment
+    # variables, which we recommend.
+    client = SoccermetricsRestClient()
 
-starters = []
-rotation_list = []
-for match in sorted_matches:
-    # Get all starting lineup information for Liverpool's matches.  We are interested
-    # in just the starting players for Liverpool, so we make player_team = 'Liverpool'
-    # and set is_starting to True.
-    lineup_data = client.link.get(match.link.lineups),
-        player_team_name="Liverpool",is_starting=True)
-    player_list = [x.player for x in lineup_data]
-    starters.append(player_list)
-    # After the first match, compute the number of players who are in the current
-    # lineup and the previous one.  Subtract that number from STARTING_XI to
-    # get the number of squad rotations.
-    if len(starters) > 1:
-        num_no_changes = len(set(starters[-2]).intersection(starters[-1]))
-        rotation_list.append(dict(date=match.match_date,
-            matchday=match.matchday,
-            rotations=STARTING_XI-num_no_changes))
+    # Create STARTING_XI constant and set it to 11.
+    # It's not necessary, but we don't like to hard-code numbers.
+    STARTING_XI = 11
 
-# Display the rotation history and a running total of squad rotations.
-print "Match\tMatch Date\tRotations\tTotal Rotations"
-cumul = []
-for datum in rotation_list:
-    cumul.append(datum['rotations'])
-    print "%d\t%s\t%d\t%d" % (datum['matchday'],datum['date'],datum['rotations'],
-                              sum(cumul))
+    # Get unique IDs of all of Liverpool's matches.  We want the matches in order,
+    # so we do a few things:
+    #   (1) We query all of Liverpool's home matches, then all of their away matches
+    #       and join the results together.
+    #   (2) We sort the results by match date.
+    matches = []
+    for key in ['home_team_name','away_team_name']:
+        param = {key: 'Liverpool'}
+        for page in iter(client.match.information.get(**param)):
+            matches.extend(page)
+    sorted_matches = sorted(matches, key=lambda k: k.match_date)
+
+    starters = []
+    rotation_list = []
+    for match in sorted_matches:
+        # Get all starting lineup information for Liverpool's matches.  We are interested
+        # in just the starting players for Liverpool, so we make player_team = 'Liverpool'
+        # and set is_starting to True.
+        lineup_data = []
+        for page in iter(client.link.get(match.link.lineups,
+                         player_team_name="Liverpool",is_starting=True)):
+            lineup_data.extend(page)
+        player_list = [x.player for x in lineup_data]
+        starters.append(player_list)
+        # After the first match, compute the number of players who are in the current
+        # lineup and the previous one.  Subtract that number from STARTING_XI to
+        # get the number of squad rotations.
+        if len(starters) > 1:
+            num_no_changes = len(set(starters[-2]).intersection(starters[-1]))
+            rotation_list.append(dict(date=match.match_date,
+                matchday=match.matchday,
+                rotations=STARTING_XI-num_no_changes))
+
+    # Display the rotation history and a running total of squad rotations.
+    print "Match\tMatch Date\tRotations\tTotal Rotations"
+    cumul = []
+    for datum in rotation_list:
+        cumul.append(datum['rotations'])
+        print "%d\t%s\t%d\t%d" % (datum['matchday'],datum['date'],datum['rotations'],
+                                  sum(cumul))
