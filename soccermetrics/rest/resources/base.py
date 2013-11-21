a106b2508060673055411ac5d60a1410a2fb1ad4
Howard Hamilton <hhamilton@soccermetrics.net>
2013-11-21 03:58:59 -0500

    Add Responses class for API response data

--- soccermetrics/rest/resources/base.py
+++ soccermetrics/rest/resources/base.py
@@ -51,10 +51,7 @@ class Resource(object):
         resp = requests.get(uri,params=full_param_dict)
 
         if resp.status_code == 200:
-            jresp = resp.json()
-            return EasyDict(dict(headers=resp.headers,
-                                 meta=jresp['meta'],
-                                 data=jresp['result']))
+            return Response(self.base_uri, self.auth, resp)
         else:
             raise SoccermetricsRestException(resp.status_code,resp.url)
 
@@ -109,10 +106,7 @@ class Resource(object):
         resp = requests.options(uri,params=self.auth)
 
         if resp.status_code == 200:
-            jresp = resp.json()
-            return EasyDict(dict(headers=resp.headers,
-                                 meta=jresp['meta'],
-                                 data=jresp['result']))
+            return Response(self.base_uri, self.auth, resp)
         else:
             raise SoccermetricsRestException(resp.status_code,resp.url)
 
@@ -177,3 +171,88 @@ class Link(Resource):
         """
         self.endpoint = uri
         return super(Link, self).options()
+
+
+class Response(Resource):
+    """
+    Represents a REST API response object.
+
+    Derived from :class:`Resource`.
+    """
+    def __init__(self, base_uri, auth, resp):
+        """
+        Constructor of Response class.
+
+        :param base_uri: Base URI of API.
+        :type base_uri: string
+        :param auth: Authentication credential.
+        :type auth: tuple
+        :param resp: response object from API request
+        :type resp: JSON
+        """
+        super(Response, self).__init__(base_uri, auth)
+        jresp = resp.json()
+        self._meta = EasyDict(jresp['meta'])
+        self.headers = EasyDict(resp.headers)
+        self.response = EasyDict(dict(data=jresp['result']))
+
+    def _iter(self):
+        """Custom iterator to retrieve all data from API response"""
+        resp = self
+        while True:
+            yield (resp.response.data)
+            if not resp._meta.next:
+                raise StopIteration
+            else:
+                resp = resp.next()
+
+    @property
+    def page(self):
+        """Current page property"""
+        return self._meta.page
+
+    @property
+    def pages(self):
+        """Total pages property"""
+        return self._meta.total_pages
+
+    @property
+    def records_page(self):
+        """Records per page property"""
+        return self._meta.records
+
+    @property
+    def records(self):
+        """Total records property"""
+        return self._meta.total_records
+
+    def first(self):
+        """Go to first page of API response"""
+        self.endpoint = self._meta.first
+        return super(Response, self).get()
+
+    def next(self):
+        """Go to next page of API response"""
+        if self._meta.next:
+            self.endpoint = self._meta.next
+            return super(Response, self).get()
+        return None
+
+    def prev(self):
+        """Go to previous page of API response"""
+        if self._meta.prev:
+            self.endpoint = self._meta.prev
+            return super(Response, self).get()
+        return None
+
+    def last(self):
+        """Go to last page of API response"""
+        self.endpoint = self._meta.last
+        return super(Response, self).get()
+
+    def all(self):
+        """Retrieve all data of API response"""
+        rec = []
+        for page in self._iter():
+            rec.extend(page)
+        return rec
