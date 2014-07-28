f143f3acb6326498271f516f3192eb9afed6e922
Howard Hamilton <hhamilton@soccermetrics.net>
2014-07-27 23:43:22 -0400

    Stub canonical endpoint test suite

---
+++ tests/test_endpoints_canonical.py
@@ -0,0 +1,9 @@
+import unittest
+import mock
+
+from soccermetrics.rest import SoccermetricsRestClient
+
+class ClientCanonicalEndpointTest(unittest.TestCase):
+    """
+    Test canonical endpoints of API resources in client.
+    """
